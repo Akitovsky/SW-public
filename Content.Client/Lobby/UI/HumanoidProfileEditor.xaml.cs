6d87a9b3bca4d2fe55244e1499d862c9f3fb1293
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2026-07-24 19:08:31 +0300

    feat: TEST pallete change

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -896,7 +896,7 @@ namespace Content.Client.Lobby.UI
 
                     category.AddChild(new PanelContainer
                     {
-                        PanelOverride = new StyleBoxFlat {BackgroundColor = Color.FromHex("#464966")},
+                        PanelOverride = new StyleBoxFlat {BackgroundColor = Color.FromHex("#41332F")},
                         Children =
                         {
                             new Label
--- Content.Client/Silicons/StationAi/StationAiFixerConsoleWindow.xaml.cs
+++ Content.Client/Silicons/StationAi/StationAiFixerConsoleWindow.xaml.cs
@@ -32,7 +32,7 @@ public sealed partial class StationAiFixerConsoleWindow : FancyWindow
 
     private readonly Dictionary<StationAiState, Color> _statusColors = new()
     {
-        [StationAiState.Empty] = Color.FromHex("#464966"),
+        [StationAiState.Empty] = Color.FromHex("#41332F"),
         [StationAiState.Occupied] = Color.FromHex("#3E6C45"),
         [StationAiState.Rebooting] = Color.FromHex("#A5762F"),
         [StationAiState.Dead] = Color.FromHex("#BB3232"),
