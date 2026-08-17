512df89b12da93c48b2a33ba4cc3e542b33b5919
dantanat1 <93005892+dantanat1@users.noreply.github.com>
2026-08-17 13:27:35 +0300

    Revert "бафф аугментум"
    This reverts commit 91ab3bbac1e29464c332d2a45a526b21c16dfe0f.

--- Content.Server/Imperial/Medieval/Magic/AugmentumBuff/AugmentumBuffOnMeleeHitComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.Imperial.Medieval.Magic.AugmentumBuff;
-
-[RegisterComponent]
-public sealed partial class AugmentumBuffOnMeleeHitComponent : Component
-{
-    [DataField(required: true)]
-    public float Duration;
-}
