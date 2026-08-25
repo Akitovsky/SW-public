f53269094acae8500d3943e8bde9c4742a19e50b
Akitovsky <kulpfedor@yandex.ru>
2026-08-26 01:27:57 +0300

    1

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -65,6 +65,7 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         Entry("!щ.", "chatsan-snaps"),
         //imperial rus replace end @keeyn and @Ser1-1y
         // I could've done this with regex, but felt it wasn't the right idea.
+        Entry(")", "chatsan-smiles"),
         Entry(":)", "chatsan-smiles"),
         Entry(":]", "chatsan-smiles"),
         Entry("=)", "chatsan-smiles"),
